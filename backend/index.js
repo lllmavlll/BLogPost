3dbe4140d6fe67aa4df17b7f4e4b68e2073235fa
Mahesh <maheshdsu86@gmail.com>
2023-05-10 11:21:01 +0530

    nodeProject

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,33 @@ const mongoose= require('mongoose')
 const cors =require('cors')
 const userRoute = require("./routes/userRoute")
 const noteRoute = require("./routes/notesRoute")
+const path =require('path')
 const app = express()
 
 
 const dotenv =require('dotenv')
 dotenv.config()
 
-const PORT = process.env.PORT || 3555
+const PORT = process.env.PORT || 3000
+
+//----- static files
+const spath = path.join(__dirname,'./public')
+app.use(express.static(spath))
 
 //----- mongo connect -----//
 
 mongoose.connect(process.env.MONGO) 
 .then(()=> {
-    app.listen(PORT,()=>{
-        console.log(`running onn port https://localhost:${PORT}`)
-    })
+    // app.listen(PORT,()=>{
+    //     console.log(`running onn port https://localhost:${PORT}`)
+    // })
 })
 .catch((err)=>console.log(err))
+
+
+app.listen(PORT,()=>{
+    console.log(`running onn port https://localhost:${PORT}`)
+})
 
 //----- creating a middleware -----//
 // app.use((req,res,next)=>{
@@ -36,8 +46,16 @@ app.use(express.json())
 //-----using imported routes-----//
 app.use('/users',userRoute)
 app.use('/notes',noteRoute)
+
+//----- view engine -----//
+const temp_path = path.join(__dirname,'./templates/views')
+app.set("view engine", "hbs")
+app.set("views",temp_path)
+
+
 app.get('/',(req,res)=>{
-    res.send("yes! is working")
+    // res.send("yes! its working")
+    res.render('index')
 });
 
 
